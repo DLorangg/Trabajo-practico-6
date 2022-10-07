2ffd64eb9fbc6f318b68e9823806db398587a72d
DLorangg <lorang.damian.epet20@gmail.com>
2022-10-07 15:27:31 -0300

    Creando function para botonArriba

---
+++ JS/script.js
@@ -0,0 +1,15 @@
+function irArriba(pixeles){
+     window.addEventListener('scroll', () => {
+        let scroll = document.documentElement.scrollTop;
+        console.log(scroll);
+        let botonArriba = document.getElementById('botonArriba')
+        if(scroll > pixeles){
+            botonArriba.style.bottom = 20 + "px";
+        }else{
+            botonArriba.style.bottom = -100 + "px";
+        }
+        
+     })
+}
+
+irArriba(400);
